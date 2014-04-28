2ef9942c73f343dae050d3b4572bc492ecf1a247
Meterbuchus93 <kbahirwani.mmo@gmail.com>
2014-04-29 02:45:47 +0530

    Solved Programming Assignment 2
    Changed the description so that it shows the Assignment has been completed.Minor comment changes to go with.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
 ## To avoid calculating the inverse of a matrix again and agian you can store it in a 
 ## retrievable cache which changes only if the matrix called changes.
-
 ## The makeCacheMatrix function is a constructor function that creates functions to set and get the matrix and
 ## the inverse of the matrix.
 
@@ -18,9 +17,9 @@ makeCacheMatrix <- function(x = matrix()) {
        getInverse = getinverse)
 }
 
-## The cacheSolve function gets the functions created in the makeCacheMatrix to retrieve the inverse of a matrix. 
-## If the matrix has not changed and the inverse has already been calculated, it retrieves the matrix inverse from
-## the cache. Otherwise, it calculates the matrix inverse and stores it in the cache.
+## The cacheSolve function takes a cacheable matrix as an input to retrieve the inverse of a matrix. 
+## If the matrix is unchanged and the inverse has already been calculated, it retrieves the matrix inverse from
+## the cache. If not, it calculates the matrix inverse and stores it in the cache.
 
 cacheSolve <- function(x, ...) {
 ## Returns the matrix that is the inverse of 'x'
